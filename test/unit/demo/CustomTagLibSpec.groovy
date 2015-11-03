9a5eb010cedba03d27d715f2e8995e63b9f23b44
Jeff Scott Brown <brownj@ociweb.com>
2015-11-03 09:23:16 -0600

    fix test

--- test/unit/demo/CustomTagLibSpec.groovy
+++ test/unit/demo/CustomTagLibSpec.groovy
@@ -4,16 +4,24 @@ import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 import asset.pipeline.grails.AssetsTagLib
+import asset.pipeline.grails.AssetMethodTagLib
 
 @TestFor(CustomTagLib)
-@Mock(AssetsTagLib)
+@Mock([AssetsTagLib, AssetMethodTagLib])
 class CustomTagLibSpec extends Specification {
 
+    static doWithSpring = {
+        assetProcessorService MockProcessor
+    }
     void "test custom tag"() {
         when:
         def output = applyTemplate('<g:myTag/>')
 
         then:
-        output == '<img src="/tagissue/assets/grails-cupsonly-log-white.svg" />'
+        output == '<img src="/some/path/grails-cupsonly-log-white.svg" />'
     }
+}
+
+class MockProcessor {
+    def assetMapping = 'some/path'
 }
